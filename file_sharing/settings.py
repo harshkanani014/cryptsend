c54606e4314a1a05df8283cdecadfdc5adee83b5
harshkanani014 <harshkanani100@gmail.com>
2021-12-04 21:07:57 +0530

    deploy

--- file_sharing/settings.py
+++ file_sharing/settings.py
@@ -87,12 +87,12 @@ WSGI_APPLICATION = 'file_sharing.wsgi.application'
 #     }
 # }
 
-# DATABASES = {
-#     'default': {
-#         'ENGINE': 'django.db.backends.sqlite3',
-#         'NAME': 'db.sqlite3',
-#     }
-# }
+DATABASES = {
+    'default': {
+        'ENGINE': 'django.db.backends.sqlite3',
+        'NAME': 'db.sqlite3',
+    }
+}
 
 import dj_database_url 
 prod_db  =  dj_database_url.config(conn_max_age=500)
